1e780595162b6abf745784b0a05e3a4b8171aed5
sihanwang <ocpwj@hotmail.com>
2020-03-22 10:18:33 +0800

    Day 15

--- src/main/java/com/sihanwang/study/spelling/LoadVcblFromFile.java
+++ src/main/java/com/sihanwang/study/spelling/LoadVcblFromFile.java
@@ -47,7 +47,7 @@ public class LoadVcblFromFile {
 		
 	;
 		
-		File file = new File("/Users/jing.wang/Desktop/word/WWW4A_DAY14.txt");
+		File file = new File("/Users/jing.wang/Desktop/word/WWW4A_DAY15.txt");
 		
 		DownloadWordList(file);
 		logger.info("Done");
