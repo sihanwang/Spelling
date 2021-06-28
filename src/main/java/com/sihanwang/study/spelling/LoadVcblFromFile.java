fda53eca19416017f1b7647c62b70eb9c5cd047c
sihanwang <ocpwj@hotmail.com>
2021-06-28 19:57:27 +0800

    new

--- src/main/java/com/sihanwang/study/spelling/LoadVcblFromFile.java
+++ src/main/java/com/sihanwang/study/spelling/LoadVcblFromFile.java
@@ -56,7 +56,7 @@ public class LoadVcblFromFile {
 	public static void main(String[] args) throws Exception {
 		// DownloadWordList(args[0]);
 		
-		File file = new File("/Users/jing.wang/github/Spelling/vocabulary/new/grade4.txt");
+		File file = new File("/Users/jing.wang/github/Spelling/vocabulary/new/houzhongyubei_unit12B.txt");
 		
 		DownloadWordList(file);
 		logger.info("Done");
